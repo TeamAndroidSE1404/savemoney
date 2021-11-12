a33196d58221aa6123584d044a83c8a0bef8b7a3
phuchell <phuchell@gmail.com>
2021-11-12 22:32:24 +0700

    [PHUC] commit AddEditCategoryActivity

--- app/src/main/java/com/app/savemoney/AddEditCategoryActivity.java
+++ app/src/main/java/com/app/savemoney/AddEditCategoryActivity.java
@@ -2,6 +2,7 @@ package com.app.savemoney;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +17,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.savemoney.adapter.AddEditCategoryAdapter;
-import com.app.savemoney.adapter.AddEditExpenseIncomeAdapter;
 import com.app.savemoney.callbacks.OnImageClickListener;
+import com.app.savemoney.common.CommonCodeValues;
+import com.app.savemoney.common.StringUtils;
+import com.app.savemoney.dao.CategoryDao;
 import com.app.savemoney.model.Category;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AddEditCategoryActivity extends AppCompatActivity implements OnImageClickListener {
@@ -31,6 +33,12 @@ public class AddEditCategoryActivity extends AppCompatActivity implements OnImag
     private ImageView btnCheckMark, btnBackHomePage, imageIcon;
     private EditText edtCateName;
     private TextView nameIcon;
+    private CategoryDao categoryDao;
+    private String userUid;
+    private String classifyCate;
+
+    public static final String CLASSIFY_CATE = "classifyCate";
+
     private OnImageClickListener onImageClickListener;
     private int[] images = {R.drawable.ic_game, R.drawable.ic_pencil_card, R.drawable.ic_remove_category,
             R.drawable.ic_gear_setting, R.drawable.ic_pencil_card, R.drawable.ic_pencil_card,
@@ -43,6 +51,18 @@ public class AddEditCategoryActivity extends AppCompatActivity implements OnImag
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sp1 = this.getSharedPreferences("Login", MODE_PRIVATE);
+
+        userUid = sp1.getString("userUid", null);
+
+        Intent intent = getIntent();
+        classifyCate = CommonCodeValues.SPENDING;
+        if(intent!=null){
+            String temp = intent.getStringExtra(CLASSIFY_CATE);
+            classifyCate = StringUtils.isEmpty(temp)?CommonCodeValues.SPENDING:temp;
+        }
+
         setContentView(R.layout.activity_add_category);
         btnCheckMark = findViewById(R.id.ic_check_mark);
         btnBackHomePage = findViewById(R.id.icon_back_category_setting);
@@ -50,22 +70,37 @@ public class AddEditCategoryActivity extends AppCompatActivity implements OnImag
         imageIcon = findViewById(R.id.iv_icon_in_add_screen);
         nameIcon = findViewById(R.id.txt_icon_name);
 
+        nameIcon.setText("icon_name_1");
+
+        categoryDao = new CategoryDao(userUid);
+
         String categoryId = getIntent().getStringExtra("categoryId");
         Toast.makeText(AddEditCategoryActivity.this, categoryId, Toast.LENGTH_SHORT).show();
+
         btnCheckMark.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AddEditCategoryActivity.this, ListExpenseIncomeActivity.class);
-                intent.putExtra("categoryName", edtCateName.getText().toString());
-                startActivity(intent);
+
+                String categoryName = edtCateName.getText().toString();
+
+                String categoryIcon = nameIcon.getText().toString();
+
+                if(StringUtils.isEmpty(categoryName)||StringUtils.isEmpty(categoryIcon)){
+                    Toast.makeText(getBaseContext(), "Please fill all information!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Category cate = new Category("", categoryName, categoryIcon, classifyCate, "0");
+                    categoryDao.addCategory(cate);
+                    finish();
+                }
+
             }
         });
 
         btnBackHomePage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AddEditCategoryActivity.this, MainActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
 
